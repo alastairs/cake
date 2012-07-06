3e045736399aae5d81333704159b4629f8661aa4
Jonny Stoten <jonathan.stoten@grantadesign.com>
2012-07-06 16:57:20 +0100

    Add solution and a bunch of empty projects
    All the projects have been left exactly as VS2012 created them

---
+++ Cake/Cake.WebApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Cake.WebApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
