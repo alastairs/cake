c14cf2f73b43e66fac946359a974a5236660fdc7
Jonny Stoten <jonathan.stoten@grantadesign.com>
2012-07-16 14:40:11 +0100

    Implement step definitions

---
+++ Cake/Cake.AcceptanceTests/WebDriverExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Cake.AcceptanceTests
+{
+    public static class WebDriverExtensions
+    {
+        public static void NavigateTo(this IWebDriver webDriver, string relativeUrl)
+        {
+            var fullUrl = UrlProvider.ApplicationBaseUrl + relativeUrl;
+            webDriver.Url = fullUrl;
+        }
+
+        public static void AssertElementIsVisible(this ISearchContext searchContext, string cssSelector)
+        {
+            IWebElement element = searchContext.Find(cssSelector);
+            Assert.That(element.Displayed, Is.True);
+        }
+
+        public static IWebElement Find(this ISearchContext searchContext, string cssSelector)
+        {
+            return searchContext.FindElement(By.CssSelector(cssSelector));
+        }
+
+        public static IEnumerable<IWebElement> FindAll(this ISearchContext searchContext, string cssSelector)
+        {
+            return searchContext.FindElements(By.CssSelector(cssSelector));
+        } 
+    }
+}
