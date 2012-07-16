42d69b39b11653a4eabe6580140b09d0e24fb55d
Jonny Stoten <jonathan.stoten@grantadesign.com>
2012-07-16 14:58:47 +0100

    Add test for HomeController

---
+++ Cake/Cake.WebApp.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Cake.WebApp.Controllers;
+using NUnit.Framework;
+
+namespace Cake.WebApp.Tests.Controllers
+{
+    [TestFixture]
+    public sealed class HomeControllerTests
+    {
+        [Test]
+        public void TestThat_Index_Should_RenderView()
+        {
+            var controller = new HomeController();
+
+            ActionResult result = controller.Index();
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+    }
+}
